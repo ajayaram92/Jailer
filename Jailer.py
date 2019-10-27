29088df9713d26137dea4429072ba27132a73a1b
ajayaram92 <44171457+ajayaram92@users.noreply.github.com>
2019-10-27 13:14:43 +0530

    Update Jailer.py

--- Jailer.py
+++ Jailer.py
@@ -1,6 +1,9 @@
+
 import random as rn
 import csv
 
+
+## Writing all the tags and their respectiuve heroes, Easier to update if there is a change in the Future, Also didnt include champion, because fuck it im not writing a seperate case just for LC
 Assasin =  {'BH','AM','NYX','QOP','MOR','PA','SLR','TA','FV'}
 Blood =  {'OGR','BS','WAR'}
 Brawny = {'AXE','BM','JUG','BB','DIS'}
@@ -27,10 +30,11 @@ Troll = {'SS','DAZ','WD','TW','BAT'}
 Warlock = {'VM','WAR','SF','ALC','BRD','DIS','WD'}
 Warrior = {'TSK','TNY','SLD','PUJ','JUG','TW','TH','KUN'}
 
+## Combining all the sets to get the total list of heroes
 Total = Assasin.union(Blood,Brawny,Brute,DeadI,Demon,Dragon,Druid,Elusive,Healer,Heart,Human,Hunter,
                     Insect,Invent,Knight,Mage,Prime,Savage,Scaled,Scrappy,Shaman,Troll,Warlock,Warrior)
 Total_heros = sorted(list(Total))
-
+## List of heroes, Alliance, Minimum required to complete highest tier, variable that stores banned heroes in a instance of jail
 tag_assasins = [list(Assasin),'Assasin',6,0]
 tag_blood = [list(Blood),'Blood Bound',2,0]
 tag_brawny = [list(Brawny),'Brawny',4,0]
@@ -56,35 +60,69 @@ tag_shaman = [list(Shaman),'Shaman',4,0]
 tag_troll = [list(Troll),'Troll',4,0]
 tag_warlock = [list(Warlock),'Warlock',6,0]
 tag_warrior = [list(Warrior),'Warrior',6,0]
+
+## combining all into a list of lists
 Tags = [tag_assasins,tag_blood,tag_brawny,tag_brute,tag_deadi,tag_demon,tag_dragon,tag_druid,tag_elusive,tag_healer,tag_heart,
       tag_human,tag_hunter,tag_insect,tag_invent,tag_knight,tag_mage,tag_prime,tag_savage
       ,tag_scaled,tag_scrappy,tag_shaman,tag_troll,tag_warlock,tag_warrior]
 
-
-
+## storing Alliances in a list
+total_tags=[]
+for tag in Tags:
+    total_tags.append(tag[1])
+## List of heroes and number of timnes banned in the MonteCarlo simulation
+final_ban =[]
+final_tag =[]
+for hero in Total_heros:
+    test=[hero,0]
+    final_ban.append(test)
+for tag in total_tags:
+    test = [tag, 0]
+    final_tag.append(test)
+## Initializing some variable/lists for calculations
 total_percentage=[]
+total_tag_percentage=[]
+## Iter= number of iterations, change depending on how many times you want to run the MC simulation (higher is better) must be a multiple of 100
 iter=1000000
-for num in range(0,iter):
+for num in range(0,iter):                                           ## loop for number of iterations
     ban = []
+    ban_tag=[]
     Avl_heros = sorted(Total_heros)
-    for i in range (0,8):
+    for i in range (0,8):                                           ## loop for banning heroes
 
-        ban.append(Avl_heros[rn.randint(0,len(Avl_heros)-1)])
+        ban.append(Avl_heros[rn.randint(0,len(Avl_heros)-1)])       ## appending ban list
         for tag in Tags:
-            if tag[0].count(ban[i])>(0):
+            if tag[0].count(ban[i])>(0):                            ## checking tags of banned hero
                tag[-1]+=1
-            #if tag[-1] > 1:
-                #print(ban, tag[1], tag[-1])
-            if tag[2] == len(tag[0]) - tag[-1]:
-                #print(tag[1],tag[2],tag[-1],num)
+               ban_tag.append(tag[1])
+
+
+
+            if tag[2] == len(tag[0]) - tag[-1]:                     ##  updating valid heroes for next ban
+
                 Avl_heros=list(set(Avl_heros)^set(tag[0]))
                 tag[-1]=0
-        for tag in Tags:
+
+        for tag in Tags:                                            ## resetting value for next ban
             tag[-1] = 0
 
+    total_tag_percentage += list(dict.fromkeys(ban_tag))            ## summing up the tags banned per cycle of Jail
+    total_percentage += ban                                         ## summing up the heroes banned per cycle of Jail
+    if (num+1) % 100 ==0:                                           ## Storing the ban amount of tags and heroes every 100 cycles of jail, Mainly used for optimization purposes
+        for hero in final_ban:
+            hero[1]+= total_percentage.count(hero[0])
+        for tag in final_tag:
+            tag[ 1] += total_tag_percentage.count(tag[0])
+        total_percentage=[]                                         ##  resetting the counting list for next 100 cycles
+        total_tag_percentage=[]                                     ##  resetting the counting list for next 100 cycles
+        print((num+1)/100)                                          ##  Just a line of code letting you know if you fucked up the optimization somewhere
 
-    total_percentage=total_percentage+ban
+## Writing CSV files for hero ban% and tag ban%
 with open(str('Iterration'+str(iter)+'jail.csv'), mode='w') as jail:
     writer = csv.writer(jail, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for hero in Total_heros:
-        writer.writerow([hero, (total_percentage.count(hero) * 100 / iter)])
+    for hero in final_ban:
+        writer.writerow([hero[0], (hero[1] * 100 / iter)])          ## writing in percentage
+with open(str('Iterration Tag'+str(iter)+'jail.csv'), mode='w') as jail:
+    writer = csv.writer(jail, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for tag in final_tag:
+        writer.writerow([tag[0], (tag[1] * 100 / iter)])            ## writing in percentage
